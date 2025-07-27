9f6359189f3f5745b954cda2d67fa3e5e8e21ea8
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-07-27 15:01:45 +0530

    created middleware to check whether the user is authorized to access a particular document

---
+++ server/middleware/documentAuthorizationCheck.js
@@ -0,0 +1,28 @@
+const Document = require("../models/document");
+
+const documentAuthorizationCheck = async (req,res,next)=>{
+    try {const documentId=req.params.docId;
+        const document=await Document.findById(documentId)
+        if (!document){
+            return res.status(404).json({message:'Document not found'})
+        }
+        if ((req.user.toString() != document.owner.toString()) && (!document.collaborators.some(id => id.toString() === req.user.toString()))){
+            const err= new Error('Not Authorized To Access')
+            err.name='NotAuthorized'
+            throw err
+        }
+        req.document = document;
+        next()
+    } catch (error) {
+        if (error.name=='NotAuthorized'){
+            return res.status(401).json({message:error.message})
+        } else if (error.name === "CastError") {
+            return res.status(400).json({ message: "Invalid ID format" });
+        }
+        console.log(error);
+        return res.status(500).json({ message: "Server error" });
+    }
+    
+}
+
+module.exports={documentAuthorizationCheck}
