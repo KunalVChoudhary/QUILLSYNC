1823104039f9a0a80ef8d2a8cfbcf10425064046
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-07-27 15:05:52 +0530

    created schema for Document Model

---
+++ server/models/document.js
@@ -0,0 +1,46 @@
+const {Schema, model} = require('mongoose')
+
+const documentSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        default:'',
+        required:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    },
+    collaborators:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'user',
+        }
+    ],
+    lastEdited:{
+        type: Date,
+        default: Date.now,
+    },
+    // history:{
+    //     type:String,
+    //     default:''
+    // }
+    history: [
+        {
+            content: String,
+            editedAt: {
+            type: Date,
+            default: Date.now
+            }
+        }
+    ]
+
+},{timestamps:true})
+
+const Document = model('document',documentSchema)
+
+module.exports=Document
