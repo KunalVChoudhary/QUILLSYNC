e938950f80c48aa8f4fbc3577d88d295ab713581
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-07-28 17:50:25 +0530

    created Authorization check for owner of document and minor fixes

--- server/middleware/documentAuthorizationCheck.js
+++ server/middleware/documentAuthorizationCheck.js
@@ -1,12 +1,14 @@
+const mongoose = require("mongoose");
 const Document = require("../models/document");
 
 const documentAuthorizationCheck = async (req,res,next)=>{
-    try {const documentId=req.params.docId;
+    try {
+        const documentId=new mongoose.Types.ObjectId(req.params.docId);
         const document=await Document.findById(documentId)
         if (!document){
             return res.status(404).json({message:'Document not found'})
         }
-        if ((req.user.toString() != document.owner.toString()) && (!document.collaborators.some(id => id.toString() === req.user.toString()))){
+        if ((req.user.userId.toString() != document.owner.toString()) && (!document.collaborators.some(id => id.toString() === req.user.toString()))){
             const err= new Error('Not Authorized To Access')
             err.name='NotAuthorized'
             throw err
@@ -25,4 +27,28 @@ const documentAuthorizationCheck = async (req,res,next)=>{
     
 }
 
-module.exports={documentAuthorizationCheck}
+const documentOwnerAuthorizationCheck= async(req,res,next)=>{
+     try {const documentId=new mongoose.Types.ObjectId(req.params.docId);
+        const document=await Document.findById(documentId)
+        if (!document){
+            return res.status(404).json({message:'Document not found'})
+        }
+        if (req.user.userId.toString() != document.owner.toString()){
+            const err= new Error('Not Authorized To Access')
+            err.name='NotAuthorized'
+            throw err
+        }
+        req.document = document;
+        next()
+    } catch (error) {
+        if (error.name=='NotAuthorized'){
+            return res.status(401).json({message:error.message})
+        } else if (error.name === "CastError") {
+            return res.status(400).json({ message: "Invalid ID format" });
+        }
+        console.log(error);
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
+module.exports={documentAuthorizationCheck, documentOwnerAuthorizationCheck}
