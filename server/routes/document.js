ab053539b3d7220d2dec94e31595a6b592056d3b
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-07-29 12:54:07 +0530

    created route to add collaborator

--- server/routes/document.js
+++ server/routes/document.js
@@ -1,11 +1,13 @@
 const {Router} = require('express')
 const { documentAuthorizationCheck, documentOwnerAuthorizationCheck } = require('../middleware/documentAuthorizationCheck')
 const { userAuthorization } = require('../middleware/userAuthorization')
-const { handleDocumentGetRequest, handleDocumentCreateRequest, handleDocumentPatchRequest, handleDocumentDeleteRequest, } = require('../controllers/document')
+const { handleDocumentGetRequest, handleDocumentCreateRequest, handleDocumentPatchRequest, handleDocumentDeleteRequest, handleAddingCollaboratorsRequest, } = require('../controllers/document')
 
 
 const route = Router()
 
+
+//document routes
 route.get('/documents/:docId', userAuthorization, documentAuthorizationCheck, handleDocumentGetRequest)
 
 route.post('/documents', userAuthorization, handleDocumentCreateRequest)
@@ -13,5 +15,9 @@ route.post('/documents', userAuthorization, handleDocumentCreateRequest)
 route.patch('/documents/:docId', userAuthorization, documentAuthorizationCheck, handleDocumentPatchRequest)
 
 route.delete('/documents/:docId', userAuthorization, documentOwnerAuthorizationCheck, handleDocumentDeleteRequest)
+
+
+//collaborators routes
+route.patch('/documents/:docId/collaborator/add', userAuthorization, documentOwnerAuthorizationCheck, handleAddingCollaboratorsRequest)
 
 module.exports= route
