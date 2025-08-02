a7230c12c9a7fbe3fb269375f928ab1513ef8951
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-08-03 00:10:19 +0530

    created handleGetAuthorizedDocumentsListRequest controller to get all the documents user is authorized to access

--- server/controllers/document.js
+++ server/controllers/document.js
@@ -1,3 +1,4 @@
+const { Types } = require("mongoose")
 const Document = require("../models/document")
 const User = require("../models/user")
 
@@ -84,4 +85,60 @@ const handleAddingCollaboratorsRequest = async (req,res)=>{
 
 
 
-module.exports={handleDocumentGetRequest, handleDocumentCreateRequest, handleDocumentPatchRequest, handleDocumentDeleteRequest, handleAddingCollaboratorsRequest}
+const handleRemovingCollaboratorsRequest = async (req,res)=>{
+    try {
+        const {collaborators} = req.body
+        const failedCollaborator=[]
+        const removeId=[]
+        if (collaborators){
+            const users=await Promise.all(
+                collaborators.map(async (email,index)=>{
+                    const user = await User.findOne({ email })
+                    if (!user){
+                            failedCollaborator.push(email)
+                    } else{
+                        removeId.push(user._id)
+                    }
+                    return true
+                    
+                })
+            )
+            const newCollaborators = req.document.collaborators.filter(
+                (id) => !removeId.some(removeIdVal => removeIdVal.equals(id))
+            );
+            const document = await Document.findOneAndUpdate({_id:req.document._id},{collaborators:newCollaborators},{new:true})
+            return res.status(200).json({document,failedCollaborator})
+
+        }} catch (error){
+            console.log(error);
+            return res.status(500).json({message:'Server Error'})
+        }
+}
+
+//Document Authorized to access controllers
+
+const handleGetAuthorizedDocumentsListRequest = async (req,res)=>{
+    try {
+        const userId = new Types.ObjectId(req.user.userId)
+        const docs = await Document.find({
+            $or: [{ owner: userId }, { collaborators: userId }]
+            });
+
+        const ownedDocs = [];
+        const collaboratorDocs = [];
+
+        docs.forEach(doc => {
+        if (doc.owner.equals(userId)) ownedDocs.push([doc._id,doc.title]);
+        else collaboratorDocs.push([doc._id,doc.title,doc.owner]);
+        });
+
+        return res.json({ ownedDocuments: ownedDocs, collaboratorDocuments: collaboratorDocs });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:'Server Error'})
+    }
+}
+
+
+
+module.exports={handleDocumentGetRequest, handleDocumentCreateRequest, handleDocumentPatchRequest, handleDocumentDeleteRequest, handleAddingCollaboratorsRequest, handleRemovingCollaboratorsRequest, handleGetAuthorizedDocumentsListRequest}
