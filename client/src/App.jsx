dfc89ea7c5ac8b1b65701a52f461e1abfde39368
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-08-04 01:08:01 +0530

    added Router,Routes,Toasts etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+//import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Routes>
+        <Route path='/register' element={
+          <div>
+            <ToastContainer position="top-right" />
+            <RegisterPage purpose='Sign Up' />
+          </div>
+        } />
+
+        <Route path='/login' element={
+          <div>
+            <ToastContainer position="top-right" />
+            <RegisterPage purpose='Sign In' />
+          </div>
+        } />
+
+      </Routes>
     </>
   )
 }
