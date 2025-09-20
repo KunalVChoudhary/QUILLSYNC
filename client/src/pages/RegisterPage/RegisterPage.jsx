691c1980d6eea4874d43859ef03998e51828a527
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-20 20:30:39 +0530

    included toastcontainer

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './RegisterPage.module.scss'
 import LoginForm from '../../components/LoginForm/LoginForm'
+import { ToastContainer } from 'react-toastify'
 
 function RegisterPage({purpose}) {
   return (
     <>
-      <div className={`${styles['login-form-container']} d-flex align-items-center`}>
-        <LoginForm purpose={purpose} />
+      <div>
+        <ToastContainer position="top-right" />
+        <div className={`${styles['login-form-container']} d-flex align-items-center`}>
+          <LoginForm purpose={purpose} />
+        </div>
       </div>
     </>
   )
