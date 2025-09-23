14a8b0ed657e73b8591b818844c6e32b43fe5d8a
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-23 10:48:10 +0530

    navbar component

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import {useState} from 'react'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import styles from './Navbar.module.scss'
+
+function Navbar(){
+
+    const navigate = useNavigate()
+
+    const userLoginState = false
+
+    const userName= 'Billu'
+    const userimage = false
+    const location = useLocation();
+    const pathSegments = location.pathname.split('/').filter(segment => segment);
+    console.log(pathSegments)
+
+    const logout = ()=>{
+
+    }
+
+
+    return(
+        <>
+            <div className={`${styles["common-navbar"]} fixed-top d-flex  w-100 justify-content-between text-white`}>
+
+                <div className={`${styles["container-1"]} d-flex h-100 w-30 px-3`}>
+                    <div className={`${styles["app-logo-container"]} d-flex align-items-center`} onClick={()=>{navigate('/')}}>
+                        <img className={`h-100 d-block px-2 py-1`} src="./assets/app_icon.png" alt="Logo"/>
+                        <p className={`${styles["app-name"]} px-1 fs-1 m-0`} >QuillSync</p>
+                    </div>                    
+                </div>
+
+                <div className={`${styles['container-2']} d-flex justify-content-between w-70 gap-2 h-100 px-3`}>
+                    <div className={`d-flex h-100 align-items-center px-3 fs-4 ${(pathSegments[0] === undefined) && styles['navItemSelected']}`} onClick={()=>{navigate('/')}}> Home </div>
+                    <div className={`d-flex h-100 align-items-center px-3 fs-4 ${(pathSegments[0]==='dashboard') && styles['navItemSelected']}`} onClick={()=>{navigate('/dashboard')}}> Dashboard </div>
+                    {
+                        userLoginState?
+                        (
+                            <div className={`${styles['profile-container']} d-flex flex-column h-100 align-items-center fs-4`}>
+                                <div className={`d-flex align-items-center gap-2 h-100 px-3`}>
+                                    <img className={` d-block h-75`} src={userimage? "profileImage" : "./images/assets/avatar_icon.png"} alt="avtar" />
+                                    <div>{userName.length<=5? userName.slice(0,5).concat(' ') : userName.slice(0,5).concat('...')}</div>
+                                </div>
+                                <div className={`${styles['dropdown-list']} flex-column h-auto w-100 border border-1 rounded fs-5`}>
+                                    <div className={`d-flex justify-content-end align-items-center pe-3 ps-4 py-2 ${(pathSegments[0]==='profile') && styles['navItemSelected']}`} onClick={()=>{navigate('/profile')}}>Profile</div>
+                                    <hr className='m-0'/>
+                                    <div className={`d-flex justify-content-end align-items-center pe-3 ps-4 py-2`} onClick={()=>{
+                                        logout()
+                                        navigate('/')
+                                    }}>Log Out</div>
+                                </div>
+                            </div>
+                        )
+                        :
+                        (
+                            <div className={`d-flex h-100 align-items-center px-3 fs-4`} onClick={()=>{navigate('/register')}}>
+                                Sign Up
+                            </div>
+                        )
+                    }
+                </div>
+
+            </div>
+        </>
+    )
+};
+
+export default Navbar;
