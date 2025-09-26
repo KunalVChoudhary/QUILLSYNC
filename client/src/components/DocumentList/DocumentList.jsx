0aa2b15a7b56bbe63ce2d16ec2d8880897e1ee2c
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-09-26 17:54:57 +0530

    created component to show the documents list in leftside bar for the dashboard

---
+++ client/src/components/DocumentList/DocumentList.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import styles from './DocumentList.module.scss'
+
+function DocumentList() {
+
+    const [ownedDocumentsArray, setOwnedDocumentsArray] = useState([['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef']])
+    const [collaboratorDocumentsArray, setCollaboratorDocumentsArray] = useState([['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef'],['asfdvrv','cawerfhbchbwkrevbeui wneocinono','fwef']])
+
+    const [dropdownStateOwnedDocuments, setDropdownStateOwnedDocuments] = useState(false)
+    const [dropdownStateCollaboratorDocuments, setDropdownStateCollaboratorDocuments] = useState(false)
+
+    const handleDocumentClick = (docId)=>{
+        return
+    }
+
+    useEffect(()=>{
+        //fetch documents from the server
+    },[])
+    
+    return (
+        <div className={`${styles['scroll-box']} overflow-y-scroll text-white`}>
+
+            {/* dono array pe map loop chalao */}
+            <div className={`${styles['doc-container']} d-flex justify-content-between px-2 my-2`}>
+                <div className='text-truncate h6'>
+                    Owned Documents
+                </div>
+                <div onClick={()=> setDropdownStateOwnedDocuments(prev=>!prev)}>
+                    {
+                        (dropdownStateOwnedDocuments)
+                        ? <img src="./assets/arrow-up-32.png" alt="up" />
+                        : <img src="./assets/arrow-down-32.png" alt="down" />
+                    }
+                </div>
+            </div>
+
+            <div className={`${dropdownStateOwnedDocuments && 'd-none'}`}>
+                {ownedDocumentsArray.map((document,index)=>{
+                    return (
+                        <div  className={` ${styles['doc-container']} d-flex justify-content-between px-2`} onClick={handleDocumentClick}>
+                            <div className='text-truncate h-100'>
+                                {index+1}• {document[1]}
+                            </div>
+                            <div className={`${styles['more-container']} d-flex flex-column align-items-end h-100`}>
+                                <div className='h-100'><img className={`${styles['dot-img']}`} src="./assets/dot-32.png" alt="more" /></div>
+                                <div className=' d-none h-100'>{document[2]}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+
+            {/* owned doc array loop */}
+
+            <div className={`${styles['doc-container']} d-flex justify-content-between px-2 my-2`}>
+                <div className='text-truncate h6'>
+                    Collaborated Documents
+                </div>
+                <div className='h-100' onClick={()=> setDropdownStateCollaboratorDocuments(prev=> !prev)}>
+                    {
+                        (dropdownStateCollaboratorDocuments)
+                        ? <img src="./assets/arrow-up-32.png" alt="up" />
+                        : <img src="./assets/arrow-down-32.png" alt="down" />
+                    }
+                </div>
+            </div>
+
+            <div className={`${dropdownStateCollaboratorDocuments && 'd-none'}`}>
+                {collaboratorDocumentsArray.map((document,index)=>{
+                    return (
+                        <div  className={` ${styles['doc-container']} d-flex justify-content-between px-2`} onClick={handleDocumentClick}>
+                            <div className='text-truncate h-100'>
+                                {index+1}• {document[1]}
+                            </div>
+                            <div className={`${styles['more-container']} d-flex flex-column align-items-end h-100`}>
+                                <div className='h-100'><img className={`${styles['dot-img']}`} src="./assets/dot-32.png" alt="more" /></div>
+                                <div className=' d-none h-100'>{document[2]}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+
+
+        </div>
+    )
+}
+
+export default DocumentList
