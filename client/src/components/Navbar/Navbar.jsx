6a65029f65d61c8b09e19243563682afc68ec45c
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-10-01 16:26:57 +0530

    using the useUserLogot hook to handle user logout and useAuth hook for userState

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,18 @@
 import {useState} from 'react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Navbar.module.scss'
+import { useAuth } from '../../hooks/useAuth'
+import useUserLogout from '../../hooks/useUserLogout'
 
 function Navbar(){
 
     const navigate = useNavigate()
-
-    const userLoginState = false
-
-    const userName= 'Billu'
+    const {user} = useAuth()
     const userimage = false
     const location = useLocation();
     const pathSegments = location.pathname.split('/').filter(segment => segment);
-    console.log(pathSegments)
 
-    const logout = ()=>{
-
-    }
+    const {logout} = useUserLogout()
 
 
     return(
@@ -34,20 +30,17 @@ function Navbar(){
                     <div className={`d-flex h-100 align-items-center px-3 fs-4 ${(pathSegments[0] === undefined) && styles['navItemSelected']}`} onClick={()=>{navigate('/')}}> Home </div>
                     <div className={`d-flex h-100 align-items-center px-3 fs-4 ${(pathSegments[0]==='dashboard') && styles['navItemSelected']}`} onClick={()=>{navigate('/dashboard')}}> Dashboard </div>
                     {
-                        userLoginState?
+                        user?
                         (
                             <div className={`${styles['profile-container']} d-flex flex-column h-100 align-items-center fs-4`}>
                                 <div className={`d-flex align-items-center gap-2 h-100 px-3`}>
                                     <img className={` d-block h-75`} src={userimage? "profileImage" : "./images/assets/avatar_icon.png"} alt="avtar" />
-                                    <div>{userName.length<=5? userName.slice(0,5).concat(' ') : userName.slice(0,5).concat('...')}</div>
+                                    <div>{user.length<=5? user : user.slice(0,5).concat('')}</div>
                                 </div>
                                 <div className={`${styles['dropdown-list']} flex-column h-auto w-100 border border-1 rounded fs-5`}>
                                     <div className={`d-flex justify-content-end align-items-center pe-3 ps-4 py-2 ${(pathSegments[0]==='profile') && styles['navItemSelected']}`} onClick={()=>{navigate('/profile')}}>Profile</div>
                                     <hr className='m-0'/>
-                                    <div className={`d-flex justify-content-end align-items-center pe-3 ps-4 py-2`} onClick={()=>{
-                                        logout()
-                                        navigate('/')
-                                    }}>Log Out</div>
+                                    <div className={`d-flex justify-content-end align-items-center pe-3 ps-4 py-2`} onClick={logout}>Log Out</div>
                                 </div>
                             </div>
                         )
