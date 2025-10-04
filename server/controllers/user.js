35b6c2773e4a5b03d4606fcb2871e0df62708da8
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-10-04 20:49:39 +0530

    added controller to check users auth

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -74,5 +74,20 @@ const handleUserLogout = (req, res) => {
     }
 };
 
+//used in check auth
+const checker=async(req,res)=>{
+    try {
+        const user = await User.findById(req.user.userId)
+        if (user){
+            return res.status(200).json({username:user.username})
+        }
+        else{
+            return res.status(400).json('USer doesnt exist')
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Server Error" });
+    }
+}
 
-module.exports={handleUserRegister,handleUserLogin,handleUserLogout}
+
+module.exports={handleUserRegister,handleUserLogin,handleUserLogout,checker}
