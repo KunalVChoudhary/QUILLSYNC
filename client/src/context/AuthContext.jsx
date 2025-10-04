97871d38d0656c250f3720fb4568a3c7713e6ab1
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-10-04 20:29:15 +0530

    using checkAuth route to set the username if user is already loggedin

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,13 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useState, useEffect  } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const storedUser = localStorage.getItem('quillsyncApp');
-  const [user, setUser] = useState(storedUser ? JSON.parse(storedUser)?.user : null );
+  const [user, setUser] = useState(null);
+  const [authLoading, setAuthLoading] = useState(true); 
+
+  const checkAuth = async()=>{
+    try{
+      const response =  await fetch(`${import.meta.env.VITE_API_URL}/api/auth/check`,{
+      method:'GET',
+      credentials:'include'
+      })
+      if (response.ok){
+        const data = await response.json()
+        setUser(data.username)
+      }else{
+        setUser(null)
+      }
+    } catch (error) {
+      console.error("Auth check failed:", error);
+      setUser(null)
+    } finally{
+      setAuthLoading(false)
+    }
+  }
+
+  //call check auth in useEffect and set tge user value to it 
+  useEffect(()=>{
+    checkAuth()
+  },[])
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, authLoading }}>
       {children}
     </AuthContext.Provider>
   );
