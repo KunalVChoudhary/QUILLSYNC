ab7c4cfa569ec2f75f3e0313f23d6f04280750e3
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-10-04 16:38:11 +0530

    using useCreateDoc hook to create a new doc at server

--- client/src/components/CreateNewDocument/CreateNewDocument.jsx
+++ client/src/components/CreateNewDocument/CreateNewDocument.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import styles from './CreateNewDocument.module.scss'
+import useCreateDoc from '../../hooks/useCreateDoc'
 
 function CreateNewDocument() {
 
-    const [createDoc, setCreateDoc] = useState(false)
+    const [showCreateDocModal, setShowCreateDocModal] = useState(false)
     const [title, setTitle] = useState('')
     const [collaborators, setCollaborators] = useState([])
     const [newCollaborator, setNewCollaborator] = useState('')
+
+    const {createDocument, loading} = useCreateDoc()
 
     const handleOnChangeTitle = (e)=>{
         setTitle(e.target.value.trim())
@@ -38,14 +41,12 @@ function CreateNewDocument() {
     }
 
     const handleSubmission = async (e)=>{
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/documents`,{
-            method:'POST',
-            headers:{
-                    'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({title,collaborators}),
-            credentials: 'include'
-        })
+        e.preventDefault()
+        
+        const result = await createDocument({title,collaborators});
+        if (result.success) {
+            setShowCreateDocModal(false);
+        }
     }
 
     //issue: not getting scrollbar for form for very very small screen  (not improtant now)
@@ -55,11 +56,11 @@ function CreateNewDocument() {
             <div className='d-flex justify-content-between text-white px-2 mt-2 border border-white mb-2'>
                 <div className='h5 m-0'> Documents </div>
                 <div className='pe-3' data-bs-toggle="tooltip" data-bs-placement="bottom" title="New Document">
-                    <i onClick={()=>setCreateDoc(true)} className="bi bi-file-earmark-richtext"></i>
+                    <i onClick={()=>setShowCreateDocModal(true)} className="bi bi-file-earmark-richtext"></i>
                 </div>
             </div>
             {
-                createDoc
+                showCreateDocModal
                 ?(
                     <>
                     <div className={`${styles['overlayBackground']} row justify-content-center align-items-center
@@ -68,7 +69,7 @@ function CreateNewDocument() {
                             <div className='d-flex justify-content-between text-white ms-md-4 align-items-start'>
                                 <div className='text-truncate h5'>Create Document</div>
                                 <div>
-                                    <img onClick={()=>{setCreateDoc(false)}} src="./assets/cancel-26.png" alt="cancel" />
+                                    <img onClick={()=>{setShowCreateDocModal(false)}} src="./assets/cancel-26.png" alt="cancel" />
                                 </div>
                             </div>
                             <form onSubmit={handleSubmission} className="mx-md-4 my-5 d-flex flex-column justify-content-center text-white">
