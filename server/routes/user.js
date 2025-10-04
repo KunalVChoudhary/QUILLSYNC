6418b26e597c3c345fe018479ff121354e7239fb
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2025-10-04 20:50:49 +0530

    created route to check user Auth

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const {Router}=require('express');
-const {handleUserRegister, handleUserLogin, handleUserLogout}=require('../controllers/user');
+const {handleUserRegister, handleUserLogin, handleUserLogout, checker}=require('../controllers/user');
 const { userAuthorization } = require('../middleware/userAuthorization');
 
 const route=Router()
@@ -9,5 +9,7 @@ route.post('/api/auth/register',handleUserRegister)
 route.post('/api/auth/login',handleUserLogin)
 
 route.get('/api/auth/logout',userAuthorization,handleUserLogout)
+
+route.get('/api/auth/check',userAuthorization,checker)
 
 module.exports=route
