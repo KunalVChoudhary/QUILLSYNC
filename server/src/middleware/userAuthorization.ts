6563fbf58c8f365084c428d29a08b0d6c7b4f82f
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2026-06-21 17:44:55 +0530

    migrated userAuthorization middleware to ts

---
+++ server/src/middleware/userAuthorization.ts
@@ -0,0 +1,18 @@
+import { checkJWT } from "../service/auth.js";
+import type { RequestHandler } from 'express';
+
+//middleware to check if client is authorized to use a certain feature
+export const userAuthorization:RequestHandler=(req,res,next)=>{
+    const token:string|undefined=req.cookies.token
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+    const decoded = checkJWT(token);
+    if (!decoded) {
+        return res.status(401).json({
+            message: 'Invalid or expired token'
+        });
+    }
+    req.user = decoded;
+    next();
+}
