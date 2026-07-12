d88601ce1aeccc327658ef86dc918b781bad7d38
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2026-07-12 16:39:39 +0530

    changed code to use reusable function documentAuthorizationService

--- server/src/middleware/documentAuthorizationCheck.ts
+++ server/src/middleware/documentAuthorizationCheck.ts
@@ -1,57 +1,91 @@
-import mongoose from "mongoose";
-import Document from "../models/document.js";
 import { type RequestHandler } from "express";
+import {
+    authorizeDocumentAccess,
+    authorizeDocumentOwner,
+} from "../services/documentAuthorizationService.js";
 
-
-export const documentAuthorizationCheck:RequestHandler = async (req,res,next)=>{
+export const documentAuthorizationCheck: RequestHandler = async (
+    req,
+    res,
+    next
+) => {
     try {
-        const document=await Document.findById(req.params.docId)
-        if (!document){
-            return res.status(404).json({message:'Document not found'})
-        }
-        if ((req.user!.userId != document.owner.toString()) && (!document.collaborators.some(id => id.toString() === req.user!.userId))){
-            const err= new Error('Not Authorized To Access')
-            err.name='NotAuthorized'
-            throw err
-        }
-        req.document = document;
-        next()
-    } catch (error) {
-        if (error instanceof Error){
-            if (error.name=='NotAuthorized'){
-                return res.status(401).json({message:error.message})
-            } else if (error.name === "CastError") {
-                return res.status(400).json({ message: "Invalid ID format" });
-            }
-        }
-        console.log(error);
-        return res.status(500).json({ message: "Server error" });
-    }
-    
-}
+        const docId = req.params.docId;
 
-export const documentOwnerAuthorizationCheck:RequestHandler = async(req,res,next)=>{
-     try {
-        const document=await Document.findById(req.params.docId)
-        if (!document){
-            return res.status(404).json({message:'Document not found'})
+        if (typeof docId !== "string") {
+            return res.status(400).json({ message: "Invalid ID format" });
         }
-        if (req.user!.userId != document.owner.toString()){
-            const err= new Error('Not Authorized To Access')
-            err.name='NotAuthorized'
-            throw err
-        }
-        req.document = document;
-        next()
+
+        req.document = await authorizeDocumentAccess(
+            req.user!.userId,
+            docId
+        );
+
+        next();
     } catch (error) {
-        if (error instanceof Error){
-            if (error.name=='NotAuthorized'){
-                return res.status(401).json({message:error.message})
-            } else if (error.name === "CastError") {
-                return res.status(400).json({ message: "Invalid ID format" });
+        if (error instanceof Error) {
+            if (error.name === "DocumentNotFound") {
+                return res.status(404).json({ message: error.message });
+            }
+
+            if (error.name === "NotAuthorized") {
+                return res.status(401).json({ message: error.message });
+            }
+
+            if (error.name === "CastError") {
+                return res.status(400).json({
+                    message: "Invalid ID format",
+                });
             }
         }
-        console.log(error);
-        return res.status(500).json({ message: "Server error" });
+
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Server error",
+        });
     }
-}
+};
+
+export const documentOwnerAuthorizationCheck: RequestHandler = async (
+    req,
+    res,
+    next
+) => {
+    try {
+        const docId = req.params.docId;
+
+        if (typeof docId !== "string") {
+            return res.status(400).json({ message: "Invalid ID format" });
+        }
+
+        req.document = await authorizeDocumentOwner(
+            req.user!.userId,
+            docId
+        );
+
+        next();
+    } catch (error) {
+        if (error instanceof Error) {
+            if (error.name === "DocumentNotFound") {
+                return res.status(404).json({ message: error.message });
+            }
+
+            if (error.name === "NotAuthorized") {
+                return res.status(401).json({ message: error.message });
+            }
+
+            if (error.name === "CastError") {
+                return res.status(400).json({
+                    message: "Invalid ID format",
+                });
+            }
+        }
+
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Server error",
+        });
+    }
+};
