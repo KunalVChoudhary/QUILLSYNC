67e2712c8c21ec60efc722f519afe35d0808b786
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2026-07-25 17:25:42 +0530

    reafactored and migrated the USERS LOGOUT api hook to Typescript

--- client/src/hooks/useUserLogout.ts
+++ client/src/hooks/useUserLogout.ts
@@ -2,17 +2,22 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "./useAuth"
 import { toast } from "react-toastify";
 
+type UserLoginResponse ={
+        success: boolean,
+        message: string,
+    }
+
 export default function useUserLogout(){
     const {setUser} = useAuth()
     const navigate = useNavigate();
 
-    const logout = async () =>{
+    const logout = async (): Promise<void> =>{
         try{
             const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/logout`,{
                 method : 'GET',
                 credentials: 'include'
             })
-            const data = await response.json()
+            const data: UserLoginResponse = await response.json()
             if (response.ok){
                 toast.success(data.message)
                 setUser(null)
